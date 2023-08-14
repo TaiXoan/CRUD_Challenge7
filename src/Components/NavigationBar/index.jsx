2c35f74cfa4c482c79cb49ededadc33b8ea8a127
unknown <Tai Xoan@pnvl109.pn.com.vn>
2023-08-15 01:07:24 +0700

    Edit photo name

--- src/Components/NavigationBar/index.jsx
+++ src/Components/NavigationBar/index.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import styles from './styles.module.css';
 
-const NavigationBar =()=> {
-  
-    return (
-      <div className={styles.navigation}>
-        <div className={styles.btn}>
-          <button>Add New</button>
-        </div>
-        <div className={styles['search-bar']}>
-          <input type="text" name="" className={styles.research} placeholder="Search name" />
-          <div className={styles['icon-search']}>
-            <img src="Images/SearchIcon.svg" alt="" />
-          </div>
+const NavigationBar = () => {
+
+  return (
+    <div className={styles.navigation}>
+      <div className={styles.btn}>
+        <button>Add New</button>
+      </div>
+      <div className={styles['search-bar']}>
+        <input type="text" name="" className={styles.research} placeholder="Search name" />
+        <div className={styles['icon-search']}>
+          <img src="Images/SearchIcon.svg" alt="" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 export default NavigationBar;
