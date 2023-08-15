a96f1bcaa13f42495ab969929f9912df5e777327
Hồ Thị Tài <tai.ho@napaglobal.com>
2023-08-15 08:57:37 +0700

    Update NoResult_Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import NavigationBar from './Components/NavigationBar';
-import CardList from './Components/CardList';
+import NavigationBar from "./Components/NavigationBar";
+import CardList from "./Components/CardList";
+// import NoResult from "./Components/NoResult";
+
 const App = () => {
   return (
     <Router>
       <div className="container">
-        <div class="header">LIST SOCIAL CARD</div>
-        <div className='navigation'>
-          <NavigationBar></NavigationBar>
-        </div>
-        <div className='formcard'>
-          <CardList></CardList>
+        <div className="header">LIST SOCIAL CARD</div>
+        <div className="content">
+          <NavigationBar />
+          <CardList />
+          {/* <NoResult /> */}
         </div>
       </div>
     </Router>
   );
-}
+};
 export default App;
