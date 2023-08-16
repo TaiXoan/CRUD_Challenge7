0fde352f53a8b46693299605c78df1528250591d
Hồ Thị Tài <tai.ho@napaglobal.com>
2023-08-16 16:41:56 +0700

    Edit DetailPage_Component

--- src/Components/DetailPage/index.jsx
+++ src/Components/DetailPage/index.jsx
@@ -1,76 +1,77 @@
-import React from 'react';
-import styles from './styles.module.css';
-
+import React from "react";
+import styles from "./styles.module.css";
 
 const DetailPage = () => {
     return (
-
-
         <div className={styles.detailContainer}>
-            <div className={styles.detailHeader}>
-                SOCIAL CARD DETAIL
-
-            </div>
-            <div className={styles.detailContent}>
+            <div className={styles.title}>SOCIAL CARD DETAIL</div>
+            <div className={styles.userInfo}>
                 <img className={styles.roundImage} src="Images/Phu.svg" alt="" />
-                <div className={styles.title}>
+                <div className={styles.userText}>
                     <div className={styles.firstname}>Binance</div>
-                    <div className={styles.birtday}>22/04/2021(day create)</div>
+                    <div className={styles.birthday}>22/04/2021 (day create)</div>
                 </div>
             </div>
-            <div className={styles.description}>
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                The point of using Lorem Ipsum is that it has a more- or-less normal distribution of letters, as opposed to using 'Content here,
-                content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum
-                as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-                versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            <div className={styles.generaldescription}>
+                It is a long established fact that a reader will be distracted by the
+                readable content of a page when looking at its layout. The point of
+                using Lorem Ipsum is that it has a more- or-less normal distribution of
+                letters, as opposed to using 'Content here, content here', making it
+                look like readable English. Many desktop publishing packages and web
+                page editors now use Lorem Ipsum as their default model text, and a
+                search for 'lorem ipsum' will uncover many web sites still in their
+                infancy. Various versions have evolved over the years, sometimes by
+                accident, sometimes on purpose (injected humour and the like).
             </div>
-            <img className={styles.heuimage} src="Images/Hue.png" alt="" />
+            <div className={styles.image}>
+                <img src="./images/Hue.png" alt="" />
+            </div>
             <div className={styles.iconheartmess}>
-                <div className={styles.hearticon}>
-                    <div><img className={styles.heart} src="Images/Heart-solid.svg" alt="" /></div>
-                    <div>2</div>
+                <div className={styles.heart}>
+                    <img src="./images/Heart-solid.svg" alt="" />
+                    <div className={styles.count}>2</div>
                 </div>
-                <div className={styles.messageicon}>
-                    <div><img className={styles.message} src="Images/Message-solid.svg" alt="" /></div>
-                    <div>4</div>
+                <div className={styles.meseges}>
+                    <img src="./images/Message-Solid.svg" alt="" />
+                    <div className={styles.count}>2</div>
                 </div>
-
             </div>
-            <hr />
-            <div className={styles.detailpage}>
-                <div className={styles.thefirst}>
-                    <div className={styles.day}>22/04/2021 (day create)</div>
-                    <div className={styles.scripts}>It is a long established fact that a reader will be distracted by
-                        the readable content of a page when looking at its layout.
-                        The point of using Lorem Ipsum is that it has a more- or-less
-                        normal distribution of letters, as opposed to using 'Content here,
-                        content here', making it look like readable English.</div>
+            <div className={styles.line}></div>
+            <div className={styles.comments}>
+                <div className={styles.day}>22/04/2021 (day create)</div>
+                <div className={styles.commentcontent}>
+                    It is a long established fact that a reader will be distracted by the
+                    readable content of a page when looking at its layout. The point of
+                    using Lorem Ipsum is that it has a more- or-less normal distribution
+                    of letters, as opposed to using 'Content here, content here', making
+                    it look like readable English.
                 </div>
-
-
-                <div className={styles.thesecond}>
-                    <div className={styles.day}>22/04/2021 (day create)</div>
-                    <div className={styles.scripts}>It is a long established fact that a reader will be distracted by
-                        the readable content of a page when looking at its layout.
-                        The point of using Lorem Ipsum is that it has a more- or-less
-                        normal distribution of letters, as opposed to using 'Content here,
-                        content here', making it look like readable English.</div>
+            </div>
+            <div className={styles.comments}>
+                <div className={styles.day}>22/04/2021 (day create)</div>
+                <div className={styles.commentcontent}>
+                    It is a long established fact that a reader will be distracted by the
+                    readable content of a page when looking at its layout. The point of
+                    using Lorem Ipsum is that it has a more- or-less normal distribution
+                    of letters, as opposed to using 'Content here, content here', making
+                    it look like readable English.
                 </div>
-
             </div>
-            <hr />
-            <div className={styles.titlecomment}>
-                Post a new coment
-            </div>
-
-            <input className={styles.commentinput} type="text" placeholder='dhjfjshfdfd'/>
-            <div className={styles.detailFooter}>
-                {/* Nội dung phần footer */}
+            <div className={styles.line}></div>
+            <div className={styles.formcomment}>
+                <div className={styles.posttext}>Post a new comment</div>
+                <div className={styles.formcontent}>
+                    <input
+                        className={styles.commentinput}
+                        type="textarea"
+                        placeholder="Add comment"
+                    />
+                    <div>
+                        <button>Post</button>
+                    </div>
+                </div>
             </div>
         </div>
-
     );
-}
-
+};
 export default DetailPage;
