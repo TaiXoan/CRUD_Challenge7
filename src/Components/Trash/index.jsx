eb9c4bd8dee7254775326a7549b323049aeeb947
unknown <Tai Xoan@pnvl109.pn.com.vn>
2023-08-16 07:22:13 +0700

    Define Trash_Component

--- src/Components/Trash/index.jsx
+++ src/Components/Trash/index.jsx
@@ -9,8 +9,17 @@ const Trash = () => {
             <div className={styles.trash}>
                 <div className={styles.trashtitle}>
                     <div className={styles.trashsubtitle}>
-                    Your about to delete a item
+                        Your about to delete a item
                     </div>
+                    <div className={styles.trash}><img src="Images/Trash.svg" alt="" /></div>
+                    <div className={styles.discription}>This will delete your item form list
+                        <div className={styles.question}>Are you sure?</div></div>
+                    <div className={styles.btn}>
+                        <button className={styles.deletebtn}>Delete</button>
+                        <button className={styles.cancelbtn}>Cancel</button>
+                    </div>
+
+
                 </div>
             </div>
         </>
